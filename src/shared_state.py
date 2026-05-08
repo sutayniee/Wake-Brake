c0a57ccebb79e3995507e70fc4a3057e1845cf71
KiarBee <kiarashdancelshamspour@gmail.com>
2026-05-08 15:33:00 +0800

    FileUpdate

--- src/shared_state.py
+++ src/shared_state.py
@@ -1,5 +1,12 @@
 import threading
 
-fatigue_level = "LOW"
-output_frame = None
 lock = threading.Lock()
+
+output_frame = None
+
+fatigue_level = "LOW"
+
+ear_value = 0.0
+eye_height_value = 0.0
+fps_value = 0.0
+bpm_value = 0.0
